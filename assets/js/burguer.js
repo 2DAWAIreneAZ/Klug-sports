ffd857830a84d094db1ecfe1005e2c4a7412c813
2DAWAIreneAZ <ialvzap0301@ieszaidinvergeles.org>
2025-12-30 23:12:37 +0100

    nav bar

--- assets/js/burguer.js
+++ assets/js/burguer.js
@@ -6,4 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
             navLinks.classList.toggle('active');
         });
     }
+    const scrollProgress = document.querySelector('.scroll-progress');
+    if (scrollProgress) {
+        window.addEventListener('scroll', () => {
+            const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            const scrollPercent = (scrollTop / scrollHeight) * 100;
+            scrollProgress.style.width = `${scrollPercent}%`;
+        });
+    }
 });
